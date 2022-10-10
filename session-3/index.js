e3ed29a7923b5bd984339d364595462cc7a34d6b
OUAKHILATOU <ouakhilatou@ouakhilatousmbp.home>
2022-10-10 19:19:24 +0100

    My first commit

--- session-3/index.js
+++ session-3/index.js
@@ -1,2 +1,14 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+let a = 5;
+const b = 10;
+let c = a + b; 
+console.log("Orginal ouput: " + c);
+
+a = 20;
+
+console.log("After 'a' has been updated: " +c)
+
+c = a + b;
+
+console.log("After 'c' has been updated to use the new 'a' value: " + c);
