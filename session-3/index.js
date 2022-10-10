0729b4c08f6e02e1406d223403dcf7bee1687866
OUAKHILATOU <ouakhilatou@ouakhilatousmbp.home>
2022-10-10 19:59:18 +0100

    Task 2 seession 3

--- session-3/index.js
+++ session-3/index.js
@@ -1,6 +1,19 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
-let a = 5;
+
+//Task 2
+
+function sayHey() {
+    console.log("Hey!");
+}
+function conversation() {
+    sayHey();
+    console.log("How are you?");
+    console.log("Goodbye");
+}
+conversation();
+//Task 1
+/*let a = 5;
 const b = 10;
 let c = a + b; 
 console.log("Orginal ouput: " + c);
@@ -11,4 +24,4 @@ console.log("After 'a' has been updated: " +c)
 
 c = a + b;
 
-console.log("After 'c' has been updated to use the new 'a' value: " + c);
+console.log("After 'c' has been updated to use the new 'a' value: " + c);*/
